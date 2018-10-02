a9a1dced413e4fe99dbface0858a75b793bb3379
eduard-ungureanu <git@wp-ninja.net>
2018-10-02 19:45:58 +0300

    Add code for the custom Image and load it to the theme

--- functions.php
+++ functions.php
@@ -15,3 +15,9 @@ function wpcninja_translation() {
 	load_child_theme_textdomain( 'et_builder', get_stylesheet_directory() . '/lang/builder/' );
 }
 add_action( 'after_setup_theme', 'wpcninja_translation' );
+
+/*Load Custom Image Module Into Divi Back end Builder */
+function divi_child_theme_add_custom_module() {
+	get_template_part( 'custom-modules/Image' );
+}
+add_action('et_builder_ready', 'divi_child_theme_add_custom_module' );
